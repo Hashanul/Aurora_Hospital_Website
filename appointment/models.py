4fd4ae2553b260a3a497b2b10f60e516a17ece9f
Hashanul <hashanulbanna4492@gmail.com>
2025-11-05 16:52:52 +0600

    add Appointment apps with models.

--- appointment/models.py
+++ appointment/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from doctors.models import Department, Doctor, Schedule
 
 
 
 class Appointment(models.Model):
     patient_name = models.CharField(max_length=255)
-    patient_name = models.CharField(max_length=15)
+    patient_phone = models.CharField(max_length=15)
     patient_email = models.EmailField(unique=True)
+    department_name = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    doctor_name = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, blank=True)
+    schedule = models.ForeignKey(Schedule, on_delete=models.SET_NULL, null=True, blank=True)
