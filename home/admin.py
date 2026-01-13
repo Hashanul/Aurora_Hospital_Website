aa7a4ed27a1b2951ee412fc06234a9d94fc628d8
Arnab Saha <arnabs929@gmail.com>
2026-01-13 12:32:32 +0600

    updated

--- home/admin.py
+++ home/admin.py
@@ -1,37 +1,62 @@
 from django.contrib import admin
-from .models import PopUp, MenuItem, MenuContent, Hero, HeroBadge, About, Health_package, Badge, AppointmentHomeImage, HomeService
+from .models import (
+    PopUp,
+    MenuItem,
+    MenuContent,
+    Hero,
+    HeroBadge,
+    About,
+    Health_package,
+    Badge,
+    AppointmentHomeImage,
+    HomeService,
+    CorporateCarousel,
+)
 from import_export.admin import ImportExportModelAdmin
-
-
 
 
 @admin.register(PopUp)
 class PopUpAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'image']
-    search_fields = ['title']
+    list_display = ["id", "title", "image"]
+    search_fields = ["title"]
 
 
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'our_mission_title', 'our_vision_title', 'video_link']
-    list_filter = ['title', 'our_mission_title', 'our_vision_title']
-    search_fields = ['title', 'our_mission_title', 'our_vision_title']
-
+    list_display = [
+        "id",
+        "title",
+        "description",
+        "our_mission_title",
+        "our_vision_title",
+        "video_link",
+    ]
+    list_filter = ["title", "our_mission_title", "our_vision_title"]
+    search_fields = ["title", "our_mission_title", "our_vision_title"]
 
 
 @admin.register(Health_package)
 class Health_packageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'gender', 'price']
-    list_filter = ['gender', 'price']
-    search_fields = ['title', 'gender', 'price']
-
-
+    list_display = ["title", "gender", "price"]
+    list_filter = ["gender", "price"]
+    search_fields = ["title", "gender", "price"]
 
 
 @admin.register(Badge)
 class BadgeAdmin(admin.ModelAdmin):
-    list_display = ['total_appointment', 'specialists', 'happy_patients', 'winning_awards', 'updated_at']
-    search_fields = ['total_appointment', 'specialists', 'happy_patients', 'winning_awards']
+    list_display = [
+        "total_appointment",
+        "specialists",
+        "happy_patients",
+        "winning_awards",
+        "updated_at",
+    ]
+    search_fields = [
+        "total_appointment",
+        "specialists",
+        "happy_patients",
+        "winning_awards",
+    ]
 
 
 # @admin.register(Facilities)
@@ -43,14 +68,18 @@ class BadgeAdmin(admin.ModelAdmin):
 
 @admin.register(AppointmentHomeImage)
 class AppointmentHomeImageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'is_active', 'created_at']
-    list_filter = ['is_active']
-    search_fields = ['title']
-    ordering = ['created_at']
+    list_display = ["title", "is_active", "created_at"]
+    list_filter = ["is_active"]
+    search_fields = ["title"]
+    ordering = ["created_at"]
 
 
 @admin.register(HomeService)
 class HomeServiceAdmin(admin.ModelAdmin):
-    list_display = ['service_title', 'service_description']
-    list_filter = ['service_title']
-    search_fields = ['service_title', 'service_description']
+    list_display = ["service_title", "service_description"]
+    list_filter = ["service_title"]
+    search_fields = ["service_title", "service_description"]
+
+@admin.register(CorporateCarousel)
+class CorporateCarouselAdmin(admin.ModelAdmin):
+    list_display = ["id", "image", "created_at"]
